36336daf19afbc7e60ed46aa169642256f807a65
Juan Lengyel <juan.lengyel@hotmail.com>
2020-08-16 23:29:05 -0500

    Added the means to handle the navigation in the app

--- src/app/store/index.js
+++ src/app/store/index.js
@@ -1,6 +1,9 @@
 import { createStore } from 'redux';
 import { defaultState } from '../../server/defaultState';
 
-export const store = createStore(function reducer(state = defaultState, action) {
+export const store = createStore(function reducer(
+  state = defaultState,
+  action
+) {
   return state;
 });
